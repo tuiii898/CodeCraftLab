df98e48cefd612ebffdc90048aa24df1c4648b75
tuiii898 <purplebushnam1311@outlook.com>
2024-05-08 05:09:55 +0700

    feat project

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-    }
-    triangle.push(row);
-  }
-  return triangle;
+function getMin(arr) {
+  return Math.min(...arr);
 }
