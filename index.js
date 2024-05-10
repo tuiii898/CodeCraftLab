7816385bf773db32d8a87e27e6e2a01efa2b799d
tuiii898 <purplebushnam1311@outlook.com>
2024-05-10 15:04:32 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const currentDate = new Date();
-console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
+function permuteUnique(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  const used = new Array(nums.length).fill(false);
+  backtrack([]);
+  return result;
+  function backtrack(permutation) {
+    if (permutation.length === nums.length) {
+      result.push([...permutation]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
+        continue;
+      used[i] = true;
+      permutation.push(nums[i]);
+      backtrack(permutation);
+      permutation.pop();
+      used[i] = false;
+    }
+  }
+}
